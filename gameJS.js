064efae41c2a5b15c397555eddff614246b3de30
Andreas Kalo <103311502+andrewkalo@users.noreply.github.com>
2023-12-05 20:02:36 +0200

    5/12/2023

--- gameJS.js
+++ gameJS.js
@@ -1,8 +1,31 @@
-var buttonL = document.getElementById("buttonL")
-var buttonR = document.getElementById("buttonR")
-buttonL.addEventListener("click",initiateBoardL)
-buttonR.addEventListener("click",initiateBoardR)
 
+
+/*document.addEventListener('DOMContentLoaded', function() {
+        // Get the table element by its id
+        var leftBoard = document.getElementById("boardL");
+
+        var table = document.createElement('TABLE');
+        table.border = '1';
+
+        var tableBody = document.createElement('TBODY');
+        table.appendChild(tableBody);
+
+        for (var i = 0; i < 10; i++) {
+            var tr = document.createElement('TR');
+            tableBody.appendChild(tr);
+
+            for (var j = 0; j < 10; j++) {
+                var td = document.createElement('TD');
+                td.width = '75';
+                td.appendChild(document.createTextNode("Cell " + i + "," + j));
+                tr.appendChild(td);
+            }
+        }
+
+        // Append the dynamically created table to the "boardL" element
+        leftBoard.appendChild(table);
+    });*/
+    
 function initiateBoardL() {
     var leftBoard = document.getElementById("boardL");
 
@@ -47,3 +70,8 @@ function initiateBoardR() {
     }
     rightBoard.appendChild(table);
 }
+
+var buttonL = document.getElementById("buttonL");
+var buttonR = document.getElementById("buttonR");
+buttonL.addEventListener("click",initiateBoardL);
+buttonR.addEventListener("click",initiateBoardR);
--- javascript.js
+++ javascript.js
@@ -1,24 +1,30 @@
 function signUp() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var passwordRepeat = document.getElementById("passwordRepeat").value;
+    var username = document.getElementById("signUpusername").value;
+    var password = document.getElementById("signUppassword").value;
+    var passwordRepeat = document.getElementById("signUppasswordRepeat").value;
 
     if (username === "" || password === "" || passwordRepeat === "") {
         window.alert("Please fill in all fields.");
         return false;
     }
+    var loginUsername = document.getElementById("loginUsername") ;
+    var loginPassword = document.getElementById("loginPassword") ;
+
+    loginUsername.value = "";
+    loginPassword.value = "";
+    
     document.getElementById('signupForm').style.display = 'none';
     document.getElementById('loginForm').style.display = 'block';
 }
 
 function cancelSignUp(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var passwordRepeat = document.getElementById("passwordRepeat").value;
+    var username = document.getElementById("signUpusername") ;
+    var password = document.getElementById("signUppassword") ;
+    var passwordRepeat = document.getElementById("signUppasswordRepeat") ;
     
-    username = "";
-    password = "";
-    passwordRepeat = "";
+    username.value  = "";
+    password.value  = "";
+    passwordRepeat.value  = "";
 }
 
 function showSignupForm() {
@@ -31,6 +37,6 @@ var cancelSignUpButton = document.getElementById("cancelSignUpbt");
 var cancelLoginButton = document.getElementById("cancelLoginbt");
  
 
-signUpButton.addEventListener("click",signUp);
-cancelSignUpButton.addEventListener("click",cancelSignUp);
-cancelLoginButton.addEventListener("click",showSignupForm);
+signUpButton.addEventListener('click',signUp);
+cancelSignUpButton.addEventListener('click',cancelSignUp);
+cancelLoginButton.addEventListener('click',showSignupForm);
